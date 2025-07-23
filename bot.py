d66387717c23f815fd68dc9d3a9e320173810be3
fka1988 <76211615+fka1988@users.noreply.github.com>
2025-07-23 05:11:10 +0500

    Update bot.py
    мод 1,7

--- bot.py
+++ bot.py
@@ -225,11 +225,13 @@ if __name__ == "__main__":
         fallbacks=[CommandHandler("reset", reset)],
         )
 
-    app.add_handler(conv_handler)
-    app.add_handler(CommandHandler("reset", reset))
+        app.add_handler(conv_handler) 
+        app.add_handler(CommandHandler("reset", reset))
 
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=PORT,
-        webhook_url=WEBHOOK_URL
-    )
+        app.run_webhook(
+            listen="0.0.0.0",
+            port=PORT,
+            path=BOT_TOKEN,  # 🔧 Это критически важно!
+            webhook_url=WEBHOOK_URL
+        )
+
